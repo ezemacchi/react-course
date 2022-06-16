86af106f37eaa3237f56dfcc7f34ad7c57cdf6fb
Ezequiel Macchi <macchimallo@gmail.com>
2022-06-16 22:30:53 +0200

    Basic way of sending information to a component using the spread operator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import AppFooter from './AppFooter';
 import AppContent from './AppContent';
+import AppHeader from './AppHeader'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -9,12 +10,15 @@ import './index.css';
 class App extends Component {
 
   render() {
+    const myProps = {
+      title: "My Cool App!",
+      subject: "My subject"
+    };
+
     return (
       <div  className='app'>
-        <div>
-          <h1>Hello, world!</h1>
-          <AppContent />
-        </div>
+        <AppHeader {...myProps} />
+        <AppContent />
         <AppFooter />
       </div>
     );
