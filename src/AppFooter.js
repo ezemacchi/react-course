3b2a820ce91a7f89ed3a5ef75138b1583fcb6ea4
Ezequiel Macchi <macchimallo@gmail.com>
2022-06-16 22:07:30 +0200

    Basic styling

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import './AppFooter.css';
 
 export default class AppFooter extends Component {
 
@@ -7,7 +8,7 @@ export default class AppFooter extends Component {
         return(
             <Fragment>
                 <hr />
-                <p>Copyright &copy; { currnetYear } Acme Ltd,</p>
+                <p className='footer'>Copyright &copy; { currnetYear } Acme Ltd,</p>
             </Fragment>
 
         );
