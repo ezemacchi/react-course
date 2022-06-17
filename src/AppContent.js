e815e07f9fd57547284b5eb2e795d9dc10836d46
Ezequiel Macchi <macchimallo@gmail.com>
2022-06-17 08:55:31 +0200

    Basic use of state within a component

--- src/AppContent.js
+++ src/AppContent.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
-  render() {
+  state = {posts: []}
 
+  fetchList = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({posts: json});
+      });
+  }
+
+  clickedItem = (x) => {
+    console.log("clicked", x)
+  }
+
+  render() {
     return (
       <p>
         This is the content.
 
         <br />
-        <button class="btn btn-primary" href="#">My Button</button>
+        <button onClick={this.fetchList} className="btn btn-primary" href="#">Fetch data</button>
+
+        <hr />
+        <ul>
+          {this.state.posts.map(c => (
+            <li key={c.id}>
+              <a href="#!" onClick={() => this.clickedItem(c.id)}>
+                {c.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </p>
     )
   }
